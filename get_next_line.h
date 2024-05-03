8056bd8d5a2fbc5fc890d5d3daee4525495a4322
Nagisa Kawaguchi <nkawaguc@c2r6s9.42tokyo.jp>
2024-05-03 22:49:05 +0900

    [Add] initial commit

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkawaguc <nkawaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 20:02:43 by nkawaguc          #+#    #+#             */
+/*   Updated: 2024/05/03 22:42:48 by nkawaguc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define BUFFER_SIZE 1024
+
+char	*get_next_line(int fd);
+
+
+#endif
